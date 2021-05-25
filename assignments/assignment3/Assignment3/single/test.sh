a5bd4b41aa750d8e800fa0c54a65f3531a72f713
Hugh <hugh.delaney@hotmail.com>
2021-05-25 16:12:38 +0100

    CUDA

---
+++ assignments/assignment3/Assignment3/single/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nval=20000
+
+[[ -e results.txt ]] && rm results.txt 
+
+for block_size in 1 2 4 8 16 32 64 128 256 512 1024
+do
+echo "Running for block size $block_size"
+./expint -n $nval -m $nval -s -v -c -k $block_size >> results.txt
+done  
